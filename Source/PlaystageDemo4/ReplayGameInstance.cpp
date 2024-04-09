498bacf95358eeeb0aae70b38c7021180b0d28c5
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-04-09 18:22:13 -0400

    Replay Game Instance 3 - Sistema Avanzado de Replay

--- Source/PlaystageDemo4/ReplayGameInstance.cpp
+++ Source/PlaystageDemo4/ReplayGameInstance.cpp
@@ -1,25 +1,128 @@
 // Copyright 2023 Biznet It
 
-
+#include "PlaystageDemo4.h"
+#include "Runtime/NetworkReplayStreaming/NullNetworkReplayStreaming/Public/NullNetworkReplayStreaming.h"
+#include "Misc/NetworkVersion.h"
 #include "ReplayGameInstance.h"
 
 UReplayGameInstance::UReplayGameInstance()
 {
-	RecordingName = "MyReplay";
-	FriendlyRecordingName = "My Replay";
+	//RecordingName = "MyReplay";
+	//FriendlyRecordingName = "My Replay";
 }
 
-void UReplayGameInstance::StartRecording()
+void UReplayGameInstance::StartRecording(FString ReplayName, FString FriendlyName)
 {
-	StartRecordingReplay(RecordingName, FriendlyRecordingName);
+	StartRecordingReplay(ReplayName, FriendlyName);
 }
 
 void UReplayGameInstance::StopRecording()
 {
-
+	StopRecordingReplay();
 }
 
-void UReplayGameInstance::StartReplay()
+void UReplayGameInstance::StartReplay(FString ReplayName)
 {
-	PlayReplay(RecordingName, nullptr);
+	PlayReplay(ReplayName);
+}
+
+void UReplayGameInstance::FindReplays()
+{
+	if (EnumerateStreamsPtr.Get())
+	{
+		const TArray<FString> extra;
+		const FEnumerateStreamsCallback callback = OnEnumerateStreamsComplete;
+		EnumerateStreamsPtr.Get()->EnumerateStreams(FNetworkReplayVersion(), int32(), FString(), extra, callback);
+	}
+}
+
+void UReplayGameInstance::RenameReplay(const FString& ReplayName, const FString& NewFriendlyReplayName)
+{
+	//Get File Info
+	FNullReplayInfo Info;
+
+	const FString DemoPath = FPaths::Combine(*FPaths::GameAgnosticSavedDir(), TEXT("Demos/"));
+	const FString StreamDirectory = FPaths::Combine(*DemoPath, *ReplayName);
+	const FString StreamFullBaseFilename = FPaths::Combine(*StreamDirectory, *ReplayName);
+	const FString InfoFilename = StreamFullBaseFilename + TEXT(".replayinfo");
+
+	TUniquePtr &InfoFileArchive(IFileManager::Get().CreateFileReader(*InfoFilename));
+
+	if (InfoFileArchive.IsValidIndex() && InfoFileArchive->TotalSize() != 0)
+	{ 
+		FString JsonString;
+		*InfoFileArchive << JsonString;
+
+		Info.FromJson(JsonString);
+		Info.bIsValid = true;
+
+		InfoFileArchive->Close();
+	}
+
+	// Set FriendlyName
+	Info.FriendlyName = NewFriendlyReplayName;
+
+	// Write File Info
+	TUniquePtr<FArchive> ReplayInfoFileAr(IFileManager::Get().CreateFileWriter(*InfoFilename));
+
+	if (ReplayInfoFileAr.IsValid())
+	{
+		FString JsonString = Info.ToJson();
+		*ReplayInfoFileAr << JsonString;
+
+		ReplayInfoFileAr->Close();
+	}
+}
+
+void UReplayGameInstance::DeleteReplay(const FString& ReplayName)
+{
+	if (EnumerateStreamsPtr.Get())
+	{
+		EnumerateStreamsPtr.Get()->DeleteFinishedStream(ReplayName, OnDeleteFinishedStreamCompleteDelegate);
+	}
+}
+
+void UReplayGameInstance::Init()
+{
+	Super::Init();
+
+	// create a ReplayStreamer for FindReplasys() and DeleteReplay(..)
+	EnumerateStreamsPtr = FNetworkReplayStreaming::Get().GetFactory().CreateReplayStreamer();
+	// Link FindReplays() delegate to function
+	OnEnumerateStreamsCompleteDelegate = FOnEnumerateStreamsComplete::CreateUObject(this, &UReplayGameInstance::OnDeleteFinishedStreamComplete);
+	// Link DeleteReplay() delegate to function
+	OnDeleteFinishedStreamCompleteDelegate = FOnDeleteFinishedStreamComplete::CreateUObject(this, &UReplayGameInstance::OnDeleteFinishedStreamComplete);
+
+	OnEnumerateStreamsCallbackDelegate = FEnumerateStreamsCallback::CreateUObject(this, &UReplayGameInstance::OnEnumerateStreamsCallback)
+}
+
+void UReplayGameInstance::OnEnumerateStreamsComplete(const TArray<FNetworkReplayStreamInfo, FDefaultAllocator> &StreamInfos)
+{
+	TArray<FS_ReplayInfo> AllReplays;
+	for (FNetworkReplayStreamInfo StreamInfo : StreamInfos)
+	{
+		if (!StreamInfo.bIsLive)
+		{
+			AllReplays.Add(FS_ReplayInfo(StreamInfo.Name, StreamInfo.FriendlyName, StreamInfo.Timestamp, StreamInfo.LengthInMS));
+		}
+	}
+	BP_OnFindReplaysComplete(AllReplays);
+}
+
+void UReplayGameInstance::OnEnumerateStreamsCallback(const TArray<FNetworkReplayStreamInfo, FDefaultAllocator>& StreamInfos)
+{
+	TArray<FS_ReplayInfo> AllReplays;
+	for (FNetworkReplayStreamInfo StreamInfo : StreamInfos)
+	{
+		if (!StreamInfo.bIsLive)
+		{
+			AllReplays.Add(FS_ReplayInfo(StreamInfo.Name, StreamInfo.FriendlyName, StreamInfo.Timestamp, StreamInfo.LengthInMS));
+		}
+	}
+	BP_OnFindReplaysComplete(AllReplays);
+}
+
+void UReplayGameInstance::OnDeleteFinishedStreamComplete(const bool bDeleteSucceeded)
+{
+	FindReplays();
 }
--- Source/PlaystageDemo4/ReplayGameInstance.h
+++ Source/PlaystageDemo4/ReplayGameInstance.h
@@ -9,6 +9,44 @@
 /**
  * 
  */
+USTRUCT(BlueprintType)
+struct FS_ReplayInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	FString ReplayName;
+
+	UPROPERTY(BlueprintReadOnly)
+	FString FriendlyName;
+
+	UPROPERTY(BlueprintReadOnly)
+	FDateTime Timestamp;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 LenghtInMS;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsValid;
+
+	FS_ReplayInfo(FString NewName, FString NewFriendlyName, FDateTime NewTimestamp, int32 NewLenghtInMS)
+	{
+		ReplayName = NewName;
+		FriendlyName = NewFriendlyName;
+		Timestamp = NewTimestamp;
+		LenghtInMS = NewLenghtInMS;
+		bIsValid = true;
+	}
+
+	FS_ReplayInfo()
+	{
+		ReplayName = "Replay";
+		FriendlyName = "Replay";
+		Timestamp = FDateTime::MinValue();
+		LenghtInMS = 0;
+		bIsValid = false;
+	}
+};
 UCLASS()
 class PLAYSTAGEDEMO4_API UReplayGameInstance : public UGameInstance
 {
@@ -17,18 +55,55 @@ class PLAYSTAGEDEMO4_API UReplayGameInstance : public UGameInstance
 public:
 	UReplayGameInstance();
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Replays")
-	FString RecordingName;
+	//UPROPERTY(EditDefaultsOnly, Category = "Replays")
+	//FString RecordingName;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Replays")
-	FString FriendlyRecordingName;
+	//UPROPERTY(EditDefaultsOnly, Category = "Replays")
+	//FString FriendlyRecordingName;
 
 	UFUNCTION(BlueprintCallable, Category = "Replays")
-	void StartRecording();
+	void StartRecording(FString ReplayName, FString FriendlyName);
 
 	UFUNCTION(BlueprintCallable, Category = "Replays")
 	void StopRecording();
 
 	UFUNCTION(BlueprintCallable, Category = "Replays")
-	void StartReplay();
+	void StartReplay(FString ReplayName);
+
+	/**  Start looking for/finding replays on the hard drive*/
+	UFUNCTION(BlueprintCallable, Category = "Replays")
+	void FindReplays();
+
+	/* Apply a new custom name to the replay (for UI only)*/
+	UFUNCTION(BlueprintCallable, Category = "Replays")
+	void RenameReplay(const FString& ReplayName, const FString& NewFriendlyReplayName);
+
+	/** Delete a previously recorded replay*/
+	UFUNCTION(BlueprintCallable, Category = "Replays")
+	void DeleteReplay(const FString& ReplayName);
+
+	virtual void Init() override;
+
+private:
+
+	// for FindReplays()
+	TSharedPtr<INetworkReplayStreamer> EnumerateStreamsPtr;
+	//TSharedPtr<FOnDeleteFinishedStreamComplete> EnumerateStreamsPtr;
+	//TSharedPtr EnumerateStreamsPtr;
+	FOnEnumerateStreamsComplete OnEnumerateStreamsCompleteDelegate;
+
+	//void OnEnumerateStreamsComplete(const TArray<FNetworkReplayStreamInfo, FDefaultAllocator> StreamInfos);
+	void OnEnumerateStreamsComplete(const TArray<FNetworkReplayStreamInfo, FDefaultAllocator>& StreamInfos);
+
+	FEnumerateStreamsCallback OnEnumerateStreamsCallbackDelegate;
+	void OnEnumerateStreamsCallback(const TArray<FNetworkReplayStreamInfo, FDefaultAllocator>& StreamInfos);
+
+	// for DeleteReplays(..)
+	FOnDeleteFinishedStreamComplete OnDeleteFinishedStreamCompleteDelegate;
+
+	void OnDeleteFinishedStreamComplete(const bool bDeleteSucceeded);
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category= "Replays")
+	void BP_OnFindReplaysComplete(const TArray<FS_ReplayInfo> &AllReplays);
 };

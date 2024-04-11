d6971caa29ca080bcfd6e26fae5a5bc8bd3629bf
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-04-11 15:43:56 -0400

    Rediseño Isla Mundo Griego 4

--- Source/PlaystageDemo4/ReplayPlayerController.cpp
+++ Source/PlaystageDemo4/ReplayPlayerController.cpp
@@ -4,15 +4,87 @@
 #include "ReplayPlayerController.h"
 #include "Engine/World.h"
 #include "Engine/DemoNetDriver.h"
+#include "TP_StrategyWithSteam.h"
 
-void AReplayPlayerController::RestartRecording()
+AReplayPlayerController::AReplayPlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	if (UWorld* World = GetWorld())
+	bShowMouseCursor = true;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
+	bShouldPerformFullTickWhenPaused = true;
+}
+
+bool AReplayPlayerController::SetCurrentReplayPausedState(bool bDoPause)
+{
+	AWorldSettings* WorldSettings = GetWorldSettings();
+	//Set MotionBlur off and Anti Aliasing to FXAA in order to bypass the pause-bug of both
+	static const auto CVarAA = IConsoleManager::Get().FindConsoleVariable(TEXT("DefaultFeature.AntiAliasing"));
+
+	static const auto CVarMB = IConsoleManager::Get().FindConsoleVariable(TEXT("DefaultFeature.MotionBlur"));
+
+	if (bDoPause)
 	{
-		/*if (UDemoNetDriver* DemoDriver = World->DemoNetDriver)*/
-		if (UDemoNetDriver* DemoDriver = World->GetDemoNetDriver())
+		PreviousAASetting = CVarAA->GetInt();
+		PreviousMBSetting = CVarMB->GetInt();
+
+		// Set MotionBlur to OFF, Anti-Aliasing to FXAA
+		CVarAA->Set(1);
+		CVarMB->Set(0);
+
+		WorldSettings->Pauser = PlayerState;
+		return true;
+	}
+	// Rest MotionBlur and AA
+	CVarAA->Set(PreviousAASetting);
+	CVarMB->Set(PreviousMBSetting);
+
+	WorldSettings->Pauser = NULL;
+	return false;
+}
+
+int32 AReplayPlayerController::GetCurrentReplayTotalTimeInSeconds() const
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver)
 		{
-			DemoDriver->GotoTimeInSeconds(0.f);
+			return GetWorld()->GetDemoNetDriver->DemoTotalTime;
+		}
+	}
+	return 0.f;
+}
+
+int32 AReplayPlayerController::GetCurrentReplayCurrentTimeInSeconds() const
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver)
+		{
+			return GetWorld()->GetDemoNetDriver->DemoCurrentTime;
+		}
+	}
+	return 0.f;
+}
+
+void AReplayPlayerController::SetCurrentReplayTimeToSeconds(int32 Seconds)
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver)
+		{
+			GetWorld()->GetDemoNetDriver->GotoTimeInSeconds(Seconds);
 		}
 	}
 }
+
+void AReplayPlayerController::SetCurrentReplayPlayRate(float PlayRate)
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver)
+		{
+			GetWorld()->GetWorldSettings()->DemoPlayTimeDilation = PlayRate;
+		}
+	}
+}
+
+
